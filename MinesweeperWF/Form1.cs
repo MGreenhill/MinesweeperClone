7eed50c1200ee5912b0ad23c23b9c91346febc33
MGreenhill <fang88@rocketmail.com>
2022-05-31 13:22:00 -0500

    Added end game functionality
    -Changed Tile struct into class to inherit from Button.
    -Updated how tiles are referenced now that they inherit from Button.
    -Changed how bombs are assigned to tiles when grid is created.
    -Tiles now check for nearby bomb tiles instead of bomb tiles adding to nearby tiles.
    -Updated event handler when clicking tile to pass along which mouse button was clicked.
    -Added Reveal method to tile for future development.
    -Added new comments for clarity

--- MinesweeperWF/Form1.cs
+++ MinesweeperWF/Form1.cs
@@ -10,57 +10,18 @@ namespace MinesweeperWF
         Size boardSize = new Size(10, 10);
         int buttonSize = 25;
 
-        Panel gameBoard = new Panel();
-
-        public struct Tile
-        {
-            public Label tileLabel;
-            public Button tileButton;
-
-            public int Width { get; }
-            public int Height { get; }
-            public Point Position { get; } 
-            public int NearbyBombs { get; set; }
-            public bool HasBomb { get; set; }
-            public String Text { get { return tileLabel.Text; } set { tileLabel.Text = value; } }
-
-            public Tile(int width, int height, Point position, int amtBombsNear, bool isBomb)
-            {
-                Width = width;
-                Height = height;
-                Position = position;
-                HasBomb = false;
-                HasBomb = isBomb;
-                NearbyBombs = 0;
-                NearbyBombs = amtBombsNear;
-
-                tileButton = new Button();
-                tileButton.Location = Position;
-                tileButton.Width = Width-1;
-                tileButton.Height = Height-1;
-                tileButton.FlatStyle = FlatStyle.Flat;
-                
-
-                tileLabel = new Label();
-                tileLabel.Location = Position;
-                tileLabel.Width = Width-3;
-                tileLabel.Height = Height-3;
-                tileLabel.BackColor = Color.LightGray;
-                tileLabel.Margin = new Padding(3);
-
-            }
-
-        }
-
+        Panel gameBoard = new Panel();//gameBoard is created outside of contructor so it can be referenced easier
 
         public Form1()
         {
             InitializeComponent();
 
+            //Gameboard setup
             gameBoard.Location = new Point(100, 75);
             gameBoard.Size = new Size(boardSize.Width * buttonSize + 5, boardSize.Height * buttonSize + 5);
             gameBoard.BorderStyle = BorderStyle.Fixed3D;
 
+            //New game button setup
             Button newBoard = new Button();
             newBoard.Location = new Point(205, 25);
             newBoard.Width = buttonSize;
@@ -69,16 +30,17 @@ namespace MinesweeperWF
             this.Controls.Add(newBoard);
             newBoard.Click += new EventHandler(NewBoard_Click);
 
+            //Create board and add it to the form
             BuildBoard(boardSize, buttonSize, amountBombs);
             this.Controls.Add(gameBoard);
         }
 
 
+
+
         //Create a new board based on difficulty selected
         public void BuildBoard(Size boardSize, int tileSize, int bombAmount)
         {
-
-            Tile[,] grid = new Tile[boardSize.Width, boardSize.Height];
 
             Random rnd = new Random();
             List<int> bombList = new List<int>();
@@ -90,44 +52,43 @@ namespace MinesweeperWF
                 if (!bombList.Contains(randNum))
                 {
                     bombList.Add(randNum);
-                    grid[randNum % boardSize.Width, randNum / boardSize.Width].HasBomb = true;
                 }
             }
 
             //Create tiles to fill the boardSize and assign bombs to the correlating tiles
+            Tile[,] grid = new Tile[boardSize.Width, boardSize.Height];
             for (int y = 0; y < boardSize.Height; y++)
             {
                 for (int x = 0; x < boardSize.Width; x++)
                 {
-                    grid[x, y] = new Tile(tileSize, tileSize, new Point(x * tileSize, y * tileSize), grid[x, y].NearbyBombs, grid[x, y].HasBomb);
-
-                    //If current tile is a bomb, increase the nearbybombs value of surrounding tiles
-                    if (grid[x, y].HasBomb)
-                    {
-                        foreach (Tuple<int, int> neighbor in SurroundingTiles(x, y, boardSize))
-                        {
-                            grid[neighbor.Item1, neighbor.Item2].NearbyBombs++;
-                        }
-                    }
-
-
+                    grid[x, y] = new Tile(tileSize, tileSize, new Point(x * tileSize, y * tileSize), bombList.Contains((y * boardSize.Width) + x));
                 }
             }
+
+            //Calculates the amount of nearby bombs for each tile
             //Adds the buttons and labels for each tile in the grid to the control board
-            //Must do this after because label text doesn't update after implemented.
             for (int y = 0; y < boardSize.Height; y++)
             {
                 for (int x = 0; x < boardSize.Width; x++)
                 {
+                    foreach (Tuple<int, int> neighbor in SurroundingTiles(x, y, boardSize))
+                    {
+                        if(grid[neighbor.Item1, neighbor.Item2].HasBomb)
+                        {
+                            grid[x, y].NearbyBombs++;
+                        }
+                    }
                     grid[x, y].Text = grid[x, y].HasBomb ? "B" : grid[x, y].NearbyBombs.ToString();
 
-                    gameBoard.Controls.Add(grid[x, y].tileButton);
-                    gameBoard.Controls.Add(grid[x, y].tileLabel);
+                    gameBoard.Controls.Add(grid[x, y]);
+                    gameBoard.Controls.Add(grid[x, y].tileLabel);  //Must do this last because label text doesn't update after implemented.
 
-                    grid[x, y].tileButton.Click += new EventHandler(Tile_Click);
+                    grid[x, y].MouseClick += (sender, MouseEventArgs) => { TileReveal_Click(sender, MouseEventArgs);  };
                 }
             }
         }
+
+
 
         //Returns a list of the positions for available surrounding tiles
         //Pass in the current tile's coordinates and the grid's size
@@ -158,22 +119,53 @@ namespace MinesweeperWF
             return neighbors;
         }
 
+
+
         private void NewBoard_Click(object sender, EventArgs e)
         {
-
             gameBoard.Controls.Clear();
             BuildBoard(boardSize, buttonSize, amountBombs);
         }
-        
-        private void Tile_Click(object sender, EventArgs e)
+
+
+
+        private void TileReveal_Click(object sender, MouseEventArgs e)
         {
-            Button clickedButton = (Button)sender;
-            clickedButton.Dispose();
+            Tile clickedButton = (Tile)sender;
+            switch (e.Button)//Checks which button is pressed
+            {
+                case MouseButtons.Left://Reveals the tile
+                    if (clickedButton.HasBomb)
+                    {
+                        foreach (Control c in gameBoard.Controls)//if tile is a bomb, reveal all tiles/game over
+                        {
+                            Tile t = c as Tile;
+                            if (t != null)
+                            {
+                                t.Reveal();
+                            }
+                        }
+                    }
+                    else
+                    {
+                        clickedButton.Reveal();
+                    }
+                    break;
+
+                //case MouseButtons.Right:
+                //    Control ctrl = (Control)clickedButton;
+                //    ctrl.BackColor = Color.Red;
+                //    break;
+            }
+            
         }
+
+
+
         private void tileButton_Paint(object sender, PaintEventArgs e)
         {
             Tile paintTile = (Tile)sender;
-            ControlPaint.DrawBorder(e.Graphics, paintTile.tileButton.ClientRectangle,
+            ControlPaint.DrawBorder(e.Graphics, paintTile.ClientRectangle,
                 SystemColors.ControlLightLight, 5, ButtonBorderStyle.Outset,
                 SystemColors.ControlLightLight, 5, ButtonBorderStyle.Outset,
                 SystemColors.ControlLightLight, 5, ButtonBorderStyle.Outset,
---
+++ MinesweeperWF/Tile.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinesweeperWF
+{
+    internal class Tile : Button
+    {
+        public Label tileLabel;
+        public Point Position { get; }
+        public int NearbyBombs { get; set; }
+        public bool HasBomb { get; set; }
+        public bool IsRevealed { get; set; }
+        public String Text { get { return tileLabel.Text; } set { tileLabel.Text = value; } }
+        
+        public Tile(int width, int height, Point position, bool isBomb)
+        {
+            Width = width;
+            Height = height;
+            Position = position;
+            HasBomb = false;
+            HasBomb = isBomb;
+            NearbyBombs = 0;
+            NearbyBombs = 0;
+        
+            
+            Location = Position;
+            Width = Width - 1;
+            Height = Height - 1;
+            FlatStyle = FlatStyle.Flat;
+
+            tileLabel = new Label();
+            tileLabel.Location = Position;
+            tileLabel.Width = Width - 3;
+            tileLabel.Height = Height - 3;
+            tileLabel.BackColor = Color.LightGray;
+            tileLabel.Margin = new Padding(3);
+        
+        }
+    
+        public void Reveal()//Hides the button of the tile and sets a boolean that can be referenced in the future.  Bool may not be needed
+        {
+            Visible = false;
+            IsRevealed = true;
+        }
+
+    }
+    
+}
